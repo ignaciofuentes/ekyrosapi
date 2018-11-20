1b8fd09efa5fb55b203ae795bbc0daa206e40ded
Ignacio Fuentes <ignaciofuentes@outlook.com>
2018-11-20 09:50:57 -0500

    my feed

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ sdk.service(function(err, flex) {
       }
     });
   }
-  flexFunctions.register("myfeedYeasssh", getMyFeed);
+  flexFunctions.register("myfeed", getMyFeed);
   flexFunctions.register("afterHospitalFetch", afterHospitalFetch);
   flexFunctions.register("beforePostReading", beforePostReading);
 });
